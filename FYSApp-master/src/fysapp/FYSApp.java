32f7a14d65421435b5e02326f3ff01d6d14addb4
marijnbakker <gek33@live.nl>
2014-11-18 15:06:08 +0100

    Revert ""
    This reverts commit bf72242b026d811edc19dad735ca3f513b528804.

--- FYSApp-master/src/fysapp/FYSApp.java
+++ FYSApp-master/src/fysapp/FYSApp.java
@@ -12,7 +12,7 @@ import javax.swing.*;
 import javax.swing.event.*;
 
 /**
- * @version 1 - lars is gay.
+ * @version 1
  * @author chrisverra, amrishheddes, stefanlobato, jerryrump, larsvanalphen,
  * marijnbakker, danielstern 
  * Doel: Het maken van een kofferapplicatie
