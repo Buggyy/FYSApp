56d38204b520eee2195e5ac92024025b536b25d1
Chris Verra <chrisverra@gmail.com>
2014-11-18 14:15:10 +0100

    bullshit verwijderd

--- FYSApp-master/src/fysapp/FYSApp.java
+++ FYSApp-master/src/fysapp/FYSApp.java
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 // lars is een retard. en chris houdt van pik.
-=======
->>>>>>> origin/master
+
 package fysapp;
 import java.awt.*;
 import java.awt.event.*;
