3d20fb383677381ba69ad8f5b7792b65a9cd462d
Buggyy <code.lobato@gmail.com>
2014-12-03 13:44:55 +0100

    getAirports method afgemaakt. pls why rafa, why u so gud. pls

--- FYSApp-master/src/model/User.java
+++ FYSApp-master/src/model/User.java
@@ -11,6 +11,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 import javax.swing.table.DefaultTableModel;
 
@@ -19,114 +21,95 @@ import javax.swing.table.DefaultTableModel;
  * @author Rafael
  */
 public class User {
-    public static int getUserid(){
+
+    public static int getUserid() {
         // Method die zorgt dat de luggageID wordt gegenereerd.
 
         int userid = 0;
-    try {
-        String sql = "SELECT userid FROM user ORDER BY userid DESC LIMIT 0 , 1";
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DatabaseManager.openConnection();
-        Statement statement = connection.createStatement();
-        PreparedStatement pst = connection.prepareStatement(sql);
-        ResultSet rs = pst.executeQuery();
-        
-        while (rs.next()) {
+        try {
+            String sql = "SELECT userid FROM user ORDER BY userid DESC LIMIT 0 , 1";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DatabaseManager.openConnection();
+            Statement statement = connection.createStatement();
+            PreparedStatement pst = connection.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
 
                 userid = rs.getInt(1);
-        }    
-        connection.close();
-    }
-    catch (Exception e)
-    {
-        e.printStackTrace();
-        
-    }
-    return userid;
+            }
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return userid;
     }
     ///////////////////////////////////////////////////////////////////////////
-    
-    
-    public static String[] getAirports(){
+
+    public static ArrayList<String> getAirports() {
         // Method die zorgt dat de luggageID wordt gegenereerd.
-        String airport[] = new String[11];
 
-    try {
-        String sql = "SELECT airportname FROM zoekjekoffer.airport";
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DatabaseManager.openConnection();
-        Statement statement = connection.createStatement();
-        PreparedStatement pst = connection.prepareStatement(sql);
-        ResultSet rs = pst.executeQuery();
-        
-        ResultSetMetaData rsmetadata = rs.getMetaData();
+        List<String> airports = new ArrayList<>();
 
-        //int columns = rsmetadata.getColumnCount();
-        //airport[] = new String[columns];
-        int columns = rsmetadata.getColumnCount();
-        
-        while (rs.next()) {
+        try {
+            String sql = "SELECT airportname FROM zoekjekoffer.airport";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DatabaseManager.openConnection();
+            Statement statement = connection.createStatement();
+            PreparedStatement pst = connection.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
 
-            for (int j = 1; j < columns; j++) {
-                airport[j] = rs.getString(j);
+            while (rs.next()) {
+                airports.add(rs.getString("airportname"));
             }
+
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+
         }
         
-        
-        connection.close();
+        return (ArrayList<String>) airports;
+
     }
-    catch (Exception e)
-    {
-        e.printStackTrace();
-        
-    }
-    
-    return airport;
-    }
-    
-    
+
     ///////////////////////////////////////////////////////////////////////////
-    public static void addUser(String getInput1,String getInput2,
-            String getInput3,String getInput4,String getInput5,String getInput6,
-            String getInput7){
+    public static void addUser(String getInput1, String getInput2,
+            String getInput3, String getInput4, String getInput5, String getInput6,
+            String getInput7) {
         // Timestamp moet nog mee worden gegeven, iemand kan dit fixen.
-        
+
         //Date date = new Date();
         //String dateString = date.toLocaleString();
         //java.sql.Timestamp date = new java.sql.Timestamp(new java.util.Date().getTime());
         int userid = getUserid() + 1;
 
-    try {
-        String sql="INSERT INTO user (userid,username,password,role,firstname,middlename,lastname,email,airportname) VALUES ("+userid+",?,?,?,?,?,?,?,?)";
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DatabaseManager.openConnection();
-        Statement statement = connection.createStatement();
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1, getInput1); // Username
-        preparedStatement.setString(2, getInput2); // Password
-        preparedStatement.setString(3, getInput3); // Role
-        preparedStatement.setString(4, getInput4); // First name
-        preparedStatement.setString(5, getInput5); // Middle name
-        preparedStatement.setString(6, getInput6); // Last name
-        preparedStatement.setString(7, getInput7); // Email
-        //preparedStatement.setString(8, getInput8); // Airport
-        preparedStatement.executeUpdate();
-        
-        //Statement statement = connection.createStatement();
+        try {
+            String sql = "INSERT INTO user (userid,username,password,role,firstname,middlename,lastname,email,airportname) VALUES (" + userid + ",?,?,?,?,?,?,?,?)";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DatabaseManager.openConnection();
+            Statement statement = connection.createStatement();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, getInput1); // Username
+            preparedStatement.setString(2, getInput2); // Password
+            preparedStatement.setString(3, getInput3); // Role
+            preparedStatement.setString(4, getInput4); // First name
+            preparedStatement.setString(5, getInput5); // Middle name
+            preparedStatement.setString(6, getInput6); // Last name
+            preparedStatement.setString(7, getInput7); // Email
+            //preparedStatement.setString(8, getInput8); // Airport
+            preparedStatement.executeUpdate();
 
-        //statement.executeUpdate(sql);
+            //Statement statement = connection.createStatement();
+            //statement.executeUpdate(sql);
+            //ResultSet resultSet = statement.executeQuery("SELECT Name FROM ComputerStatus");
+            connection.close();
 
-        //ResultSet resultSet = statement.executeQuery("SELECT Name FROM ComputerStatus");
-        
-        connection.close();
-      
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
 
     }
-    catch (Exception e)
-    {
-        e.printStackTrace();
-        
-    }
-
-}
 }
