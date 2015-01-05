6da810941ec8ab6f5cc930c666cf909b82ccbb2c
sterndm <stern.dms@gmail.com>
2015-01-05 13:32:09 +0100

    Loginscreen TAB
    als je tab drukt, springt ie naar volgende textfield

--- FYSApp-master/src/view/LoginScreen.java
+++ FYSApp-master/src/view/LoginScreen.java
@@ -3,6 +3,8 @@ package view;
 import connectivity.DatabaseManager;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.nio.channels.SelectionKey;
 import java.sql.*;
 import main.FYSApp;
 import view.admin.AdminFront;
@@ -72,6 +74,11 @@ public class LoginScreen extends javax.swing.JPanel {
         userNameJTextField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 userNameJTextFieldActionPerformed(evt);
+            }
+        });
+        userNameJTextField.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                userNameJTextFieldKeyPressed(evt);
             }
         });
 
@@ -233,6 +240,13 @@ public class LoginScreen extends javax.swing.JPanel {
     private void cantLoginJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cantLoginJButtonActionPerformed
         FYSApp.getInstance().showPanel(new CantLogin());
     }//GEN-LAST:event_cantLoginJButtonActionPerformed
+
+    private void userNameJTextFieldKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_userNameJTextFieldKeyPressed
+       
+        if(evt.getKeyCode() == KeyEvent.VK_TAB){
+            passJTextField.setFocusTraversalKeysEnabled(true);
+        }
+    }//GEN-LAST:event_userNameJTextFieldKeyPressed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
