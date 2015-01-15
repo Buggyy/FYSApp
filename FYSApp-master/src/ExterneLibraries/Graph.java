7cf23d9a9c4c73511b023d9c790077461ac61df5
sterndm <stern.dms@gmail.com>
2015-01-15 18:11:41 +0100

    Revert "Revert "Revert "ditto"""
    This reverts commit 1bcb1b1c6b7fb38435256404f60f56a4cdf11e3a.

--- FYSApp-master/src/ExterneLibraries/Graph.java
+++ FYSApp-master/src/ExterneLibraries/Graph.java
@@ -11,28 +11,24 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 
 /**
  *
- * @author team 1 IS106
+ * @author team 1
  */
 public class Graph extends ApplicationFrame {
 
     private DefaultCategoryDataset dataset;
     private JFreeChart chart;
 
-    /**
-     *
-     * @param windowTitle
-     */
     public Graph(String windowTitle) {
-
         super(windowTitle);
         this.dataset = new DefaultCategoryDataset();
     }
 
     /**
-     *
+     * 
      * @param dateRange
      * @param list1
      * @param list2
@@ -41,59 +37,50 @@ public class Graph extends ApplicationFrame {
      * @param listName1
      * @param listName2
      * @param listName3
-     * @param listName4
+     * @param listName4 
      */
-    public void addSeriesMonths(String dateType, int beginMonth,
-            String[] dateRange, ArrayList<Integer> list1,
-            ArrayList<Integer> list2, ArrayList<Integer> list3,
+    public void addSeriesMonths(String dateType, int beginMonth, String[] dateRange, ArrayList<Integer> list1, 
+            ArrayList<Integer> list2, ArrayList<Integer> list3, 
             ArrayList<Integer> list4, String listName1, String listName2,
             String listName3, String listName4) {
 
-        LuggageManager luggageMgr = FYSApp.getLuggageManager();
-
-        for (int i = 0; i < dateRange.length; i++) {
-
-            list1.add(luggageMgr.FindNumberOfStatusByMonth(i + beginMonth,
-                    listName1));
-            list2.add(luggageMgr.FindNumberOfStatusByMonth(i + beginMonth,
-                    listName2));
-            list3.add(luggageMgr.FindNumberOfStatusByMonth(i + beginMonth,
-                    listName3));
-            list4.add(luggageMgr.FindNumberOfStatusByMonth(i + beginMonth,
-                    listName4));
+        
+        LuggageManager luggageMgr =  FYSApp.getLuggageManager();
+        
+        for (int i = 0 ; i < dateRange.length ; i ++) {
+            
+            list1.add(luggageMgr.FindNumberOfStatusByMonth(i + beginMonth, listName1));
+            list2.add(luggageMgr.FindNumberOfStatusByMonth(i + beginMonth, listName2));
+            list3.add(luggageMgr.FindNumberOfStatusByMonth(i + beginMonth, listName3));
+            list4.add(luggageMgr.FindNumberOfStatusByMonth(i + beginMonth, listName4));
         }
-
+        
         for (int i = 0; i < dateRange.length; i++) {
-
+            
             this.dataset.addValue(list1.get(i), listName1, dateRange[i]);
             this.dataset.addValue(list2.get(i), listName2, dateRange[i]);
             this.dataset.addValue(list3.get(i), listName3, dateRange[i]);
             this.dataset.addValue(list4.get(i), listName4, dateRange[i]);
         }
     }
-
-    public void addSeriesDays(String dateType, int beginMonth,
-            String[] dateRange, ArrayList<Integer> list1,
-            ArrayList<Integer> list2, ArrayList<Integer> list3,
+    public void addSeriesDays(String dateType, int beginMonth, String[] dateRange, ArrayList<Integer> list1, 
+            ArrayList<Integer> list2, ArrayList<Integer> list3, 
             ArrayList<Integer> list4, String listName1, String listName2,
             String listName3, String listName4) {
 
-        LuggageManager luggageMgr = FYSApp.getLuggageManager();
-
-        for (int i = 0; i < dateRange.length; i++) {
-
-            list1.add(luggageMgr.FindNumberOfStatusByDay(i, i + beginMonth,
-                    listName1));
-            list2.add(luggageMgr.FindNumberOfStatusByDay(i, i + beginMonth,
-                    listName2));
-            list3.add(luggageMgr.FindNumberOfStatusByDay(i, i + beginMonth,
-                    listName3));
-            list4.add(luggageMgr.FindNumberOfStatusByDay(i, i + beginMonth,
-                    listName4));
+        
+        LuggageManager luggageMgr =  FYSApp.getLuggageManager();
+        
+        for (int i = 0 ; i < dateRange.length ; i ++) {
+            
+            list1.add(luggageMgr.FindNumberOfStatusByDay(i, i + beginMonth, listName1));
+            list2.add(luggageMgr.FindNumberOfStatusByDay(i, i + beginMonth, listName2));
+            list3.add(luggageMgr.FindNumberOfStatusByDay(i, i + beginMonth, listName3));
+            list4.add(luggageMgr.FindNumberOfStatusByDay(i, i + beginMonth, listName4));
         }
-
+        
         for (int i = 0; i < dateRange.length; i++) {
-
+            
             this.dataset.addValue(list1.get(i), listName1, dateRange[i]);
             this.dataset.addValue(list2.get(i), listName2, dateRange[i]);
             this.dataset.addValue(list3.get(i), listName3, dateRange[i]);
@@ -111,19 +98,68 @@ public class Graph extends ApplicationFrame {
      * @param height height of the graph
      * @param bgColor background color of the graph
      */
-    public ChartPanel createChart(String title, String xAxisString,
-            String yAxisString, int width, int height, Paint bgColor) {
+    public ChartPanel createChart(String title, String xAxisString, String yAxisString, int width, int height, Paint bgColor) {
 
-        this.chart = ChartFactory.createLineChart(title, xAxisString,
-                yAxisString, this.dataset, PlotOrientation.VERTICAL, true, true,
-                false);
-
+        this.chart = ChartFactory.createLineChart(title, xAxisString, yAxisString, this.dataset,
+                PlotOrientation.VERTICAL, true, true, false);
         chart.setBackgroundPaint(bgColor);
         ChartPanel chartPanel = new ChartPanel(this.chart);
         chartPanel.setPreferredSize(new Dimension(width, height));
         setContentPane(chartPanel);
         pack();
-
         return chartPanel;
+
+// See http://www.java2s.com/Code/Java/Chart/JFreeChartLineChartDemo1.htm
+        // See http://www.java2s.com/Code/Java/Chart/CatalogChart.htm
+        // See http://www.jfree.org/jfreechart/api/javadoc/index.html?overview-summary.html
+        // TODO 2: create chart (this can be a line chart or a stacked bar chart)
+        // hier komt de code van opdracht 6
+    }
+
+    /**
+     * Changes thickness of series line
+     *
+     * @param seriesIndex index of series
+     * @param thickness thickness of line
+     */
+    public void setSeriesThickness(int seriesIndex, int thickness) {
+        // See http://www.jfree.org/jfreechart/api/javadoc/org/jfree/chart/renderer/category/CategoryItemRenderer.html
+        // TODO 3: minor changes: change thickness of series
+        // hint a: use chart.getPlot() to get the CategoryPlot.
+        // hint b: then use plot.getRenderer to get the CategoryItemRenderer
+        // hint c: then use the correct method of the renderer
+
+        // hier komt de code van opdracht 7
+    }
+
+    /**
+     * Changes color of series
+     *
+     * @param seriesIndex index of series
+     * @param color the chosen color
+     */
+    public void setSeriesColor(int seriesIndex, Paint color) {
+        // See http://www.jfree.org/jfreechart/api/javadoc/org/jfree/chart/renderer/category/CategoryItemRenderer.html
+        // TODO 4: minor changes: change color of series
+        // hint a: use chart.getPlot() to get the CategoryPlot.
+        // hint b: then use plot.getRenderer to get the CategoryItemRenderer
+        // hint c: then use the correct method of the renderer
+
+        // hier komt de code van opdracht 8
+    }
+
+    /**
+     * Changes colors of the plot
+     *
+     * @param bgColor the wanted background color
+     * @param gridlineColor the color if the horizontal grid lines
+     */
+    public void setGraphBackgroundColors(Paint bgColor, Paint gridlineColor) {
+        // See http://www.jfree.org/jfreechart/api/javadoc/org/jfree/chart/plot/CategoryPlot.html
+        // TODO 5: minor changes: change background color of the plot
+        // hint a: use chart.getPlot() to get the CategoryPlot.
+        // hint b: then use the correct methods of the plot
+
+        // hier komt de code van opdracht 9
     }
 }
